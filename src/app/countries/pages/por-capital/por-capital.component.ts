4799e623e601d76087c711af0c43616727e0b93e
Mauricio Belduque Guzman <mbelduqu@bancolombia.com.co>
2022-02-19 20:42:15 -0500

    add capital city impl

--- src/app/countries/pages/por-capital/por-capital.component.ts
+++ src/app/countries/pages/por-capital/por-capital.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {Pais} from "../../interfaces/pais.interface";
+import {PaisService} from "../../services/pais.service";
 
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
   styleUrls: ['./por-capital.component.css']
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
 
-  constructor() { }
+  capital: string = '';
+  hayError: boolean = false;
+  paises: Pais[] = [];
 
-  ngOnInit(): void {
+  constructor(private paisService: PaisService) {
+  }
+
+  buscar(capital: string): void {
+    this.hayError = false;
+    this.capital = capital;
+    this.paisService.buscarCapital(this.capital).subscribe((paises) => {
+      this.paises = paises;
+    }, (() => {
+      this.hayError = true;
+      this.paises = [];
+    }));
   }
 
 }
