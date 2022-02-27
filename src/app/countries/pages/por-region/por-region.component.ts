2f370c89e3e01e0d86b90775b963dd12f333ae1f
Mauricio Belduque Guzman <mbelduqu@bancolombia.com.co>
2022-02-26 23:03:45 -0500

    add: by region search impl

--- src/app/countries/pages/por-region/por-region.component.ts
+++ src/app/countries/pages/por-region/por-region.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {PaisService} from '../../services/pais.service';
+import {Pais} from "../../interfaces/pais.interface";
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styleUrls: ['./por-region.component.css']
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
 
-  constructor() { }
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+  region: string = '';
+  paises: Pais[] = [];
 
-  ngOnInit(): void {
+  constructor(private paisService: PaisService) {
+  }
+
+  activarRegion(region: string) {
+    if (region === this.regionActiva) {
+      return;
+    }
+    this.regionActiva = region;
+    this.region = region;
+    this.paises = [];
+    this.paisService.buscarPaisesRegion(this.region).subscribe((paises) => {
+      this.paises = paises;
+    })
   }
 
 }
