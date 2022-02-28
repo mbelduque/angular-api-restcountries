b1a699b170413f23860058c2d3e281266a40a210
Mauricio Belduque Guzman <mbelduqu@bancolombia.com.co>
2022-02-27 19:09:25 -0500

    add: suggestion countries feature

--- src/app/countries/pages/por-pais/por-pais.component.ts
+++ src/app/countries/pages/por-pais/por-pais.component.ts
@@ -13,23 +13,37 @@ export class PorPaisComponent {
   pais: string = '';
   hayError: boolean = false;
   paises: Pais[] = [];
+  paisesSugeridos: Pais[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) {
   }
 
   buscar(pais: string): void {
     this.hayError = false;
+    this.mostrarSugerencias = false;
     this.pais = pais;
     this.paisService.buscarPais(this.pais).subscribe((paises) => {
       this.paises = paises;
-    }, (() => {
+    }, () => {
       this.hayError = true;
       this.paises = [];
-    }));
+    });
   }
 
-  sugerencias(event: string) {
+  sugerencias(pais: string) {
     this.hayError = false;
+    this.mostrarSugerencias = true;
+    this.pais = pais;
+    this.paisService.buscarPais(pais).subscribe(paises => {
+      this.paisesSugeridos = paises.splice(0, 5)
+    }, () => {
+      this.paisesSugeridos = []
+    })
+  }
+
+  buscarSugerido(pais: string) {
+    this.buscar(pais);
   }
 
 }
